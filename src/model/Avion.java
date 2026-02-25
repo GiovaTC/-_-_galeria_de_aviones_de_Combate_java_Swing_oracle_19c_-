e9b1aa9772de56959e5f509f533bf1324c543afa
gtapiero <giovanny.tapiero@pi.edu.co>
2026-02-25 18:38:26 -0500

    commit - Avion .

--- src/model/Avion.java
+++ src/model/Avion.java
@@ -1,4 +1,27 @@
 package model;
 
+import java.awt.Image;
+
 public class Avion {
-}
+
+    private String nombre;
+    private String pais;
+    private String tipo;
+    private String velocidad;
+    private Image imagen;
+
+    public Avion(String nombre, String pais,
+                 String tipo, String velocidad, Image imagen) {
+        this.nombre = nombre;
+        this.pais = pais;
+        this.tipo = tipo;
+        this.velocidad = velocidad;
+        this.imagen = imagen;
+    }
+
+    public String getNombre() { return nombre; }
+    public String getPais() { return pais; }
+    public String getTipo() { return tipo; }
+    public String getVelocidad() { return velocidad; }
+    public Image getImagen() { return imagen; }
+}   
