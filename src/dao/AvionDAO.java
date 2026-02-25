fb9bda6f0734c0007c3b80b2d45b2fce45fa87fa
gtapiero <giovanny.tapiero@pi.edu.co>
2026-02-25 18:54:33 -0500

    commit - AvionPanel .

--- src/dao/AvionDAO.java
+++ src/dao/AvionDAO.java
@@ -36,4 +36,4 @@ public class AvionDAO {
         }
         return lista;
     }
-}   
+}
--- src/ui/AvionPanel.java
+++ src/ui/AvionPanel.java
@@ -1,4 +1,32 @@
 package ui;
 
-public class AvionPanel {
-}
+import model.Avion;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class AvionPanel extends JPanel {
+
+    public AvionPanel(Avion avion) {
+        setLayout(new BorderLayout());
+        setBorder(BorderFactory.createLineBorder(Color.GRAY));
+
+        ImageIcon icon = new ImageIcon(
+                avion.getImagen().getScaledInstance(
+                        150, 100, Image.SCALE_SMOOTH
+                )
+        );
+
+        JLabel imgLabel = new JLabel(icon, JLabel.CENTER);
+        JLabel txt = new JLabel(
+                "<html><b>" + avion.getNombre() + "</b><br>" +
+                        avion.getPais() + "<br>" +
+                        avion.getTipo() + "<br>" +
+                        avion.getVelocidad() + "</html>",
+                JLabel.CENTER
+        );
+
+        add(imgLabel, BorderLayout.CENTER);
+        add(txt, BorderLayout.SOUTH);
+    }
+} 
